ab843aaecfa02a6d1d9164d39eca959a49ef2d59
wcy21 <a18325513428@163.com>
2022-05-14 19:50:58 +0800

    2022.5.14

---
+++ BASIC_LEVEL_CPP/1001.cpp
@@ -0,0 +1,30 @@
+/********************************************************************************
+* @author: 兔子
+* @project: BASIC_LEVEL_CPP 
+* @date: 2022/5/14 19:27
+* @version: 1.0
+* @description: 1001 害死人不偿命的(3n+1)猜想 (分数 15)
+********************************************************************************/
+#if 0
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n = 0, cnt = 0;
+    cin >> n;
+    while (n != 1) {
+        if (n & 1) {
+            n = 3 * n + 1;
+        }
+        n >>= 1;
+        cnt++;
+    }
+    cout << cnt << endl;
+
+    return 0;
+}
+
+
+#endif
---
+++ BASIC_LEVEL_CPP/1002.cpp
@@ -0,0 +1,41 @@
+/********************************************************************************
+* @author: 兔子
+* @project: BASIC_LEVEL_CPP 
+* @date: 2022/5/14 19:33
+* @version: 1.0
+* @description: 1002 写出这个数 (分数 20)
+********************************************************************************/
+#if 0
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string str[10] = {
+        "ling", "yi", "er", "san", "si", "wu", "liu", "qi", "ba", "jiu"
+};
+
+void printSum(int sum) {
+    if (sum < 10) {
+        cout << str[sum];
+    } else {
+        printSum(sum / 10);
+        cout << ' ' << str[sum % 10];
+    }
+}
+
+int main() {
+    string num;
+    cin >> num;
+
+    int sum = 0;
+    for (int i = 0; i < num.length(); ++i) {
+        sum += num[i] - '0';
+    }
+    printSum(sum);
+
+    return 0;
+}
+
+#endif

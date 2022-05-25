075c574f7458e2b30d09ea77041b95bf56989468
wcy21 <a18325513428@163.com>
2022-05-25 23:55:56 +0800

    2022.5.25

--- BASIC_LEVEL_CPP/src/1024.cpp
+++ BASIC_LEVEL_CPP/src/1024.cpp
@@ -1,23 +1,23 @@
 /********************************************************************************
 * @author: 兔子
 * @project: BASIC_LEVEL_CPP 
-* @date: 2022/5/24 17:14
+* @date: 2022/5/24 18:20
 * @version: 1.0
-* @description: 组个最小数 (分数 20)
+* @description: 科学计数法 (分数 20)
 ********************************************************************************/
 
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
 int main() {
-    int x = 0, a[10];
-    for (int i = 0; i < 10; ++i) cin >> a[i];
-    while (!a[++x]);
-    cout << x, a[x]--;
-    for (int i = 0; i < 10; ++i) {
-        while (a[i]--) cout << i;
-    }
+    string str;
+    cin >> str;
+    string sa = str.substr(0, str.find('E'));
+    string sn = str.substr(str.find('E') + 1);
+    int n = stoi(sn);
+    cout << sa << ' ' << sn << endl;
+
 
     return 0;
 }

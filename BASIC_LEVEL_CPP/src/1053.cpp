89e85764b832fbb894f926e444f0f4c07de5b3c2
wcy21 <a18325513428@163.com>
2022-07-18 21:05:33 +0800

    2022.7.18

--- BASIC_LEVEL_CPP/src/1053.cpp
+++ BASIC_LEVEL_CPP/src/1053.cpp
@@ -7,10 +7,23 @@
 ********************************************************************************/
 
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 int main() {
+    int N, D, K, cnt, may_empty = 0, empty = 0;
+    double e, E;
+    cin >> N >> e >> D;
+    for (int i = 0; i < N; ++i, cnt = 0) {
+        cin >> K;
+        for (int j = 0; j < K; ++j) {
+            cin >> E;
+            if (E < e) ++cnt;
+        }
+        if (cnt > K / 2) K > D ? empty++ : may_empty++;
+    }
+    cout << fixed << setprecision(1) << may_empty * 100.0 / N << "% " << empty * 100.0 / N << "%" << endl;
 
     return 0;
 }
---
+++ BASIC_LEVEL_CPP/src/1054.cpp
@@ -0,0 +1,26 @@
+/********************************************************************************
+* @author: 兔子
+* @project: BASIC_LEVEL_CPP 
+* @date: 2022/7/18 20:36
+* @version: 1.0
+* @description: 求平均值 (分数 20)
+********************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N, cnt = 0;
+    double num, sum;
+    cin >> N;
+    for (int i = 0; i < N; ++i) {
+        string str, inte, frac;
+        cin >> str;
+        for (int j = 0; j < str.size(); ++j) {
+            
+        }
+    }
+
+    return 0;
+}
